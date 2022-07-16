830df84a85c11a6ee422b62449a81ca5291e4229
Tech0tron Roblox <tech0tron.roblox@gmail.com>
2022-07-16 13:36:53 -0600

    scaffolding pt. 2

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
+import { ControllerScheme } from './ControllerScheme/ControllerScheme';
+import { Dashboard } from './Dashboard/Dashboard';
 import './index.css';
+import { LiveRecord } from './LiveRecord/LiveRecord';
 import reportWebVitals from './reportWebVitals';
 
+type Page = "LiveRecord" | "Dashboard" | "ControllerScheme"
+
+function App() {
+  const [page, setPage] = useState<Page>("LiveRecord");
+
+  let toRender;
+  if (page === "LiveRecord") {
+    toRender = <LiveRecord/>
+  } else if (page === "ControllerScheme") {
+    toRender = <ControllerScheme/>
+  } else {
+    toRender = <Dashboard/>
+  }
+  return (
+    <React.StrictMode>
+      <div className='flex header'>
+        <button onClick={(event) => { setPage("LiveRecord") }} className={page === "LiveRecord" ? "headerButtonActivated" : "headerButton"}>LiveRecord</button>
+        <button onClick={(event) => { setPage("Dashboard") }} className={page === "Dashboard" ? "headerButtonActivated" : "headerButton"}>Dashboard</button>
+        <button onClick={(event) => { setPage("ControllerScheme") }} className={page === "ControllerScheme" ? "headerButtonActivated" : "headerButton"}>ControllerScheme</button>
+      </div>
+
+      {toRender}
+    </React.StrictMode>
+  )
+}
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <div className='flex header'>
-      <button className='headerButton'>LiveRecord</button>
-      <button className='headerButtonActivated'>Dashboard</button>
-      <button className='headerButton'>ControllerScheme</button>
-
-    </div>
-  </React.StrictMode>
+  <App />
 );
 
 // If you want to start measuring performance in your app, pass a function
